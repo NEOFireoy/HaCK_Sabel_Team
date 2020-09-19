7ef9db6bbedcd72adcd58b3840c2af9c91839a94
NEOFireoy <53156346+NEOFireoy@users.noreply.github.com>
2020-09-18 21:36:21 -0700

    Update mapping_code.py

--- mapping_code.py
+++ mapping_code.py
@@ -1,1 +1,14 @@
 import numpy
+import serial
+import time
+import sys
+import signal
+
+def signal_handler(signal, frame):
+  print("closing program")
+  SerialPort.close()
+  sys.exit(0)
+
+COM = input("Enter the COM Port\n")
+BAUD = input("Enter the Baudrate\n")
+SerialPort = serial.Serial(COM,BAUD,timeout=1)
