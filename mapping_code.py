d7288bacfc23c124bbdac3c80a125ace98e9d4f6
NEOFireoy <53156346+NEOFireoy@users.noreply.github.com>
2020-09-19 12:45:03 -0700

    Update mapping_code.py

--- mapping_code.py
+++ mapping_code.py
@@ -10,20 +10,25 @@ def signal_handler(signal, frame):
     SerialPort.close()
     sys.exit(0)
 
+map = np.zeros((5, 5))
+posx = 1
+posy = 0
+print(map)
 
 COM = input("Enter the COM Port\n")
 BAUD = 9600
 SerialPort = serial.Serial(COM, BAUD, timeout=1)
 
-posx = 1
-posy = 0
 
-#map = np.zeros(5, 5)
+
+distances = []
+
 # map is divided into 30cmx30cm submaps
 
-while (1):
+while (len(distances) < 5):
+
     try:
-        OutgoingData = 'F'
+        OutgoingData = '1'
         SerialPort.write(bytes(OutgoingData, 'utf-8'))
         time.sleep(0.001)
     except KeyboardInterrupt:
@@ -33,6 +38,26 @@ while (1):
 
     IncomingData = SerialPort.readline()
     if (IncomingData):
-        print((IncomingData).decode('utf-8'))
+        data = IncomingData.decode('utf-8')
+        distances.append(data)
         time.sleep(0.001)
 
+
+
+
+
+
+'''
+while (1):
+  try:
+     OutgoingData=input('> ')
+     SerialPort.write(bytes(OutgoingData,'utf-8'))
+  except KeyboardInterrupt:
+     print("Closing and exiting the program")
+     SerialPort.close()
+     sys.exit(0)
+  IncomingData=SerialPort.readline()
+  if(IncomingData):
+     print((IncomingData).decode('utf-8'))
+  time.sleep(0.01)
+'''
