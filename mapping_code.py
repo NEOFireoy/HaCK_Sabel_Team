0e8055cf6bc1d72b267abd74663f2408316f0400
NEOFireoy <53156346+NEOFireoy@users.noreply.github.com>
2020-09-19 15:58:35 -0700

    Update mapping_code.py

--- mapping_code.py
+++ mapping_code.py
@@ -11,8 +11,6 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 
-
-
 map = np.zeros((5, 5))
 '''
 Map: 
@@ -31,32 +29,37 @@ Map:
 '''
 posx = 1
 posy = 4
-print(map)
+# print(map)
+
+print('attempting to connect')
 
 COM = 'COM4'
 BAUD = 9600
 SerialPort = serial.Serial(COM, BAUD, timeout=1)
 
-
-
-distances = np.array((0,0,0,0,), dtype='float')
+distances = np.array((0, 0, 0, 0,), dtype='float')
 start_time = time.time()
 # map is divided into 30cmx30cm submaps
 el_time = 0.000
-while (el_time < 30.000):
-
+print('connected')
+while (el_time < 10.000):
+    print('good connect')
     try:
         OutgoingData = '1'
+        print('good out')
         SerialPort.write(bytes(OutgoingData, 'utf-8'))
         time.sleep(0.001)
     except KeyboardInterrupt:
+        print('bad out')
         print("Closing and exiting the program")
         SerialPort.close()
         sys.exit(0)
     num = 0
+    print('good send')
     while (num < 4):
         IncomingData = SerialPort.readline()
         if (IncomingData):
+            print('good read')
             data = IncomingData.decode('utf-8')
             distances[num] = float(data)
             num = num + 1
@@ -64,70 +67,40 @@ while (el_time < 30.000):
 
     if distances[1] > 30.00 and distances[2] > 30.00:
         ### Moving above or below
+        print('good data')
         try:
             OutgoingData = 'F'
+            print('good send after data')
             SerialPort.write(bytes(OutgoingData, 'utf-8'))
             time.sleep(0.001)
         except KeyboardInterrupt:
+            prin('bad send after data')
             print("Closing and exiting the program")
             SerialPort.close()
             sys.exit(0)
 
         dist_travel = SerialPort.readline()
         dist_travel = dist_travel.decode('utf-8')
+        print('good dist read')
         while float(dist_travel) < 30.00:
+            print('good dist usage')
+            print(dist_travel)
             dist_travel = SerialPort.readline()
             dist_travel = dist_travel.decode('utf-8')
 
         try:
+            print('stopping')
             OutgoingData = '1'
             SerialPort.write(bytes(OutgoingData, 'utf-8'))
             time.sleep(0.001)
         except KeyboardInterrupt:
+            print('bad stop')
             print("Closing and exiting the program")
             SerialPort.close()
             sys.exit(0)
 
-
-    '''
-    elif distances[1] > 30.00 and distances[0] > 30.00:
-        ### Turning right relative to the robot
-        try:
-            OutgoingData = 'R'
-            SerialPort.write(bytes(OutgoingData, 'utf-8'))
-            time.sleep(0.001)
-        except KeyboardInterrupt:
-            print("Closing and exiting the program")
-            SerialPort.close()
-            sys.exit(0)
-
-    elif distances[2] > 30.00 and distances[3] > 30.00:
-        try:
-            OutgoingData = 'L'
-            SerialPort.write(bytes(OutgoingData, 'utf-8'))
-            time.sleep(0.001)
-        except KeyboardInterrupt:
-            print("Closing and exiting the program")
-            SerialPort.close()
-            sys.exit(0)
-    '''
     el_time = time.time() - start_time
 
 final_time = time.time() - start_time
-print(final_time)
 
 
-'''
-while (1):
-  try:
-     OutgoingData=input('> ')
-     SerialPort.write(bytes(OutgoingData,'utf-8'))
-  except KeyboardInterrupt:
-     print("Closing and exiting the program")
-     SerialPort.close()
-     sys.exit(0)
-  IncomingData=SerialPort.readline()
-  if(IncomingData):
-     print((IncomingData).decode('utf-8'))
-  time.sleep(0.01)
-'''
