465aa2d80fcda81617f314fb52efe72ac0dd82b1
NEOFireoy <53156346+NEOFireoy@users.noreply.github.com>
2020-09-19 14:38:50 -0700

    Update bluetooth_code_ardunio_good.ino

--- bluetooth_code_ardunio_good/bluetooth_code_ardunio_good.ino
+++ bluetooth_code_ardunio_good/bluetooth_code_ardunio_good.ino
@@ -1,35 +1,4 @@
 
-/*char Incoming_value = 0;    
-
-
-void setup() 
-{ 
-    Serial.begin(9600);//Sets the baud rate for serial data       
-         //transmission 
-}
-
-void loop() 
-{
-
-
-if(Serial.available() > 0) 
-  {
-    float val = 30.00;
-    char cmd = Serial.read(); 
-    if (cmd == 's')
-    {
-      Serial.println(val);
-      delay(100);
-    }
-  }
-
-  
-
-  
-
-}*/
-
-
 const int trig_fowl = 22;
 const int echo_fowl = 23;
 
