361a238134fcbc650d62e3d02f320a03168b0d12
NEOFireoy <53156346+NEOFireoy@users.noreply.github.com>
2020-09-19 10:21:53 -0700

    Update bluetooth_code_ardunio_good.ino

--- bluetooth_code_ardunio_good/bluetooth_code_ardunio_good.ino
+++ bluetooth_code_ardunio_good/bluetooth_code_ardunio_good.ino
@@ -130,6 +130,22 @@ void rotate_left()
   //rotate left by 90deg code here
 }
 
+void initial_distance()
+{
+  double right_dist = dist_right();
+  double front_right_dist = dist_fowr();
+  double front_left_dist = dist_fowl();
+  double left_dist = dist_left();
+
+  Serial.println(right_dist);
+  delay(1);
+  Serial.println(front_right_dist);
+  delay(1);
+  Serial.println(front_left_dist);
+  delay(1);
+  Serial.println(left_dist);
+  delay(1);
+}
 
 void setup() 
 { 
@@ -155,6 +171,9 @@ void loop()
 {
   if(Serial.available() > 0)
   {
+  
+      initial_distance();
+    
       char cmd = Serial.read();
 
       if(cmd == 'F')
@@ -166,6 +185,16 @@ void loop()
         double right_dist = dist_right();
         double front_right_dist = dist_fowr();
         double front_left_dist = dist_fowl();
+        double left_dist = dist_left();
+
+        Serial.println(right_dist);
+        delay(1);
+        Serial.println(front_right_dist);
+        delay(1);
+        Serial.println(front_left_dist);
+        delay(1);
+        Serial.println(left_dist);
+        delay(1);
  
         
       }
