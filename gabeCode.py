f187ac9fcb2eba0134ce11fd98188a2c3fce7b65
NEOFireoy <53156346+NEOFireoy@users.noreply.github.com>
2020-09-19 18:26:57 -0700

    Update gabeCode.py

--- gabeCode.py
+++ gabeCode.py
@@ -33,7 +33,7 @@ posy = 4
 
 print('attempting to connect')
 
-COM = '/dev/tty.HC-05-DevB'
+COM = 'COM4'
 BAUD = 9600
 SerialPort = serial.Serial(COM, BAUD, timeout=1)
 
@@ -44,10 +44,10 @@ start_time = time.time()
 # map is divided into 30cmx30cm submaps
 el_time = 0.000
 print('connected')
-while (el_time < 10.000):
+while (el_time < 300.000):
 
     try:
-        OutgoingData = '1'
+        OutgoingData = 'S'
         print('good out')
         SerialPort.write(bytes(OutgoingData, 'utf-8'))
         time.sleep(0.001)
@@ -59,12 +59,13 @@ while (el_time < 10.000):
     num = 0
     print('good send')
     IncomingData = SerialPort.readline()
-    distances = [0,0, 0, 0]
+    distances = [0,0,0,0]
     if (IncomingData):
         print('good read')
         data = IncomingData.decode('utf-8')
         distances = data.split('s')
         time.sleep(0.01)
+    print(data)
     distances[0] = float(distances[0])
     distances[1] = float(distances[1])
     distances[2] = float(distances[2])
@@ -73,8 +74,19 @@ while (el_time < 10.000):
     average_distance = (distances[1] + distances[2])/2
     print(average_distance)
 
-    if average_distance > .3000:
-        ### Moving above or below
+    if average_distance < .1000:
+        try:
+            OutgoingData = 'S'
+            print('good send after data')
+            SerialPort.write(bytes(OutgoingData, 'utf-8'))
+            time.sleep(0.001)
+        except KeyboardInterrupt:
+            print('bad send after data')
+            print("Closing and exiting the program")
+            SerialPort.close()
+            sys.exit(0)
+
+        '''        ### Moving above or below
         print('good data')
         try:
             OutgoingData = 'F'
@@ -87,28 +99,29 @@ while (el_time < 10.000):
             SerialPort.close()
             sys.exit(0)
 
+
         dist_travel = SerialPort.readline()
-        dist_travel = dist_travel.decode('utf-8')
-        print('good dist read')
-        while float(dist_travel) < .3000:
-            print('good dist usage')
-            print(dist_travel)
-            dist_travel = SerialPort.readline()
+        if (dist_travel):
             dist_travel = dist_travel.decode('utf-8')
+            print('good dist read')
+            while float(dist_travel) < .3000:
+                print('good dist usage')
+                print(dist_travel)
+                dist_travel = SerialPort.readline()
+                dist_travel = dist_travel.decode('utf-8')
 
-        try:
-            print('stopping')
-            OutgoingData = '1'
-            SerialPort.write(bytes(OutgoingData, 'utf-8'))
-            time.sleep(0.001)
-        except KeyboardInterrupt:
-            print('bad stop')
-            print("Closing and exiting the program")
-            SerialPort.close()
-            sys.exit(0)
-
+            try:
+                print('stopping')
+                OutgoingData = '1'
+                SerialPort.write(bytes(OutgoingData, 'utf-8'))
+                time.sleep(0.001)
+            except KeyboardInterrupt:
+                print('bad stop')
+                print("Closing and exiting the program")
+                SerialPort.close()
+                sys.exit(0)
+'''
 
 
     el_time = time.time() - start_time
 
-final_time = time.time() - start_time
